961277aed18e3aded49a328ca9ede37c33c7dee3
Emanuel <emanuel.magxothwa@gmail.com>
2016-10-05 00:47:35 +0200

    A pop-up of all products is showing on new customer page

--- public/selectizejs/custom-select.js
+++ public/selectizejs/custom-select.js
@@ -1,27 +1,82 @@
 $(document).ready(function(){
 
-    $('#product-list').click(function(){
+    var selected_products = {};
+
+    function add_product(product, res_map, $input){
+
+        if (res_map[product.product_name] == undefined)
+            res_map[product.product_name] = product;
+        
+        var keys = Object.keys(res_map);        
+        var str = res_map[keys[0]].product_name;
+
+        for(var k = 1; k < keys.length; k++){            
+            str += " ," + res_map[keys[k]].product_name;
+        }
+
+        return str;
+    }
+
+    function remove_product(product_name, res_map, $input) {
+
+        if (res_map[product_name] !== undefined)
+            delete res_map[product_name];
+        
+        var keys = Object.keys(res_map);        
+        var str = res_map[keys[0]].product_name;
+
+        for(var k = 1; k < keys.length; k++){            
+            str += " ," + res_map[keys[k]].product_name;
+        }
+
+        return str;
+    }
+
+    function list_products($target_div, product_list) {
+        
+        for (var i = 0; i < product_list.length; i++) {
+            // create the necessary elements
+            var label= document.createElement("label");
+            var description = document.createTextNode(product_list[i].product_name);
+            var checkbox = document.createElement("input");
+
+            checkbox.type = "checkbox";    // make the element a checkbox
+            checkbox.name = "slct[]";      // give it a name we can check on the server side
+            checkbox.value = product_list[i].product_name;         // make its value "pair"
+
+            label.appendChild(checkbox);   // add the box to the element
+            label.appendChild(description);// add the description to the element
+
+            // add the label element to your div
+            $target_div[0].appendChild(label);
+        }
+
+    }
+
+    if($('#products').length){
         $.get('/api/products', function(options){
 
-            var docFrag = document.createDocumentFragment();
-
-            for(var i = 0; i < options.length; i++){
-
-                var newDiv = document.createElement('div');
-                newDiv.className = 'col-lg-2';
+            list_products($("#product_list"), options);
                 
-                var input = document.createElement('input');
-                input.type = 'checkbox';
-                input.value = options[i].product_name;
-                newDiv.appendChild(input);
+            var curr_prods = [];
+            $('#products').val(JSON.stringify(options));
 
-                docFrag.appendChild(newDiv);
-
-            }
-
-            document.getElementsByTagName('body')[0].appendChild(docFrag);
+            var returned = "";
+            returned = add_product(options[0], selected_products, $('#products'));
+            returned = add_product(options[1], selected_products, $('#products'));
             
-        });
-    });
+            returned = remove_product(options[0].product_name, selected_products, $('#products'));
+            returned = add_product(options[0], selected_products, $('#products'));
 
+            $("#products").val(returned);
+
+            list_products($("#list-here"), options);
+
+        });
+    };
+
+    $("#products").click(function(){
+        console.log("Nothing to see here...")
+        $("#productsList").modal("show");
+    });
 });
